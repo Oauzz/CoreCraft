ec23bdef17c96d75c7af5b3d65f987b10bf198b3
Anass Serroukh <anasssrk1@gmail.com>
2024-04-19 16:38:52 +0100

    Corrected the text centering in s4_otp_code layout
    Concerning s4_otp_code layout, ProgressBar was removed (Commented out)
    due to performance reasons and the otp code input (EditText) was
    replaced by a list of EditText with the right centering

--- app/src/main/java/com/corecraft/s4.java
+++ app/src/main/java/com/corecraft/s4.java
@@ -15,12 +15,14 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class s4 extends AppCompatActivity {
 
     final int max_len = 6;
-    EditText Code;
+    EditText[] Codes;
     TextView Error;
     Button Continue;
 
@@ -31,16 +33,14 @@ public class s4 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.s4_otp_code);
 
-        final DisplayMetrics dims = Resources.getSystem().getDisplayMetrics();
-
-        float textSizePx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,  20.0f, dims);
-        float letterSpacingPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PT,  1.8f, dims);
-        float androidLetterSpacing = letterSpacingPx / textSizePx;
-
-
-        Code = (EditText)findViewById(R.id.s4_otp_code_txt);
-        System.out.println("Coeff " + androidLetterSpacing);
-        Code.setLetterSpacing(androidLetterSpacing);
+        Codes = new EditText[]{
+                (EditText) findViewById(R.id.s4_otp_code_txt_1),
+                (EditText) findViewById(R.id.s4_otp_code_txt_2),
+                (EditText) findViewById(R.id.s4_otp_code_txt_3),
+                (EditText) findViewById(R.id.s4_otp_code_txt_4),
+                (EditText) findViewById(R.id.s4_otp_code_txt_5),
+                (EditText) findViewById(R.id.s4_otp_code_txt_6)
+        };
         Error = (TextView)findViewById(R.id.s4_error_label);
         Continue = (Button)findViewById(R.id.s4_continue_btn);
         tiles = new ImageView[]{
@@ -52,12 +52,15 @@ public class s4 extends AppCompatActivity {
                 (ImageView) findViewById(R.id.s4_code_floor_6)
         };
 
-        Code.addTextChangedListener(new OtpWatcher());
+        for(int i = 0;i < Codes.length;++i){
+            Codes[i].addTextChangedListener(new OtpWatcher(i));
+        }
+
         Continue.setOnClickListener(this::onClick);
     }
 
     void onClick(View v){
-        final String input = Code.getText().toString();
+        final String input = Arrays.stream(Codes).map(Code -> Code.getText().toString()).collect(Collectors.joining());
         if(input.length() != 6){
             Error.setText(R.string.invalid_otp_code_error);
         }else{
@@ -67,6 +70,10 @@ public class s4 extends AppCompatActivity {
 
 
     private class OtpWatcher implements TextWatcher{
+        final int n;
+        OtpWatcher(int n){
+            this.n = n;
+        }
 
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -75,17 +82,21 @@ public class s4 extends AppCompatActivity {
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            if(count < before){
-                for(int i = before + start - 1;i >= start;--i){
-                    tiles[i].setAlpha(0.5f);
+            if(count > 0 && before == 0){
+                tiles[n].setAlpha(1.0f);
+                if(n+1 < max_len){
+                    Codes[n+1].requestFocus();
+                    if(Codes[n+1].getText().toString().length() != 0)
+                        Codes[n+1].setSelection(1);
                 }
-            }else if(count > before){
-                for(int i = start;i < start + count;++i){
-                    tiles[i].setAlpha(1.0f);
+            }else if(count == 0 && before == 1){
+                tiles[n].setAlpha(0.5f);
+                if(n-1 >= 0){
+                    Codes[n-1].requestFocus();
+                    if(Codes[n-1].getText().toString().length() != 0)
+                        Codes[n-1].setSelection(1);
                 }
             }
-
         }
 
         @Override
